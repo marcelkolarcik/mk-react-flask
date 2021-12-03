4ad5e62cb073ebb297b822bc19e7452da99e2cf9
marcelkolarcik <marcelkolarcik@gmail.com>
2021-12-03 14:43:35 +0000

    added basic footer

--- frontend/src/components/pages/Footer.js
+++ frontend/src/components/pages/Footer.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 export default function Footer() {
     return (
-        <div className="row bg-black mt-5 px-5">
+        <div className="row g-0 bg-black mt-5 px-5">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <p className="col-md-4 mb-0 text-muted">&copy; 2021 Demo App, Inc</p>
                 <Link

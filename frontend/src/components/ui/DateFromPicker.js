91113571ad1cc4bd35321f13f08907264ccfd9fe
marcelkolarcik <marcelkolarcik@gmail.com>
2021-12-06 15:25:36 +0000

    fixed datepicker error, updating BookingContext inside useEffect()

--- frontend/src/components/ui/DateFromPicker.js
+++ frontend/src/components/ui/DateFromPicker.js
@@ -1,18 +1,25 @@
-import React, {useState, forwardRef, useContext} from 'react';
+import React, {forwardRef, useContext, useEffect, useState} from 'react';
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 import BookingContext from "../../store/booking-context";
+
 export default function DateFormPicker(props) {
     const bookingCtx = useContext(BookingContext)
     const [date, setDate] = useState(new Date());
+
     const ExampleCustomInput = forwardRef(({value, onClick}, ref) => (
         <button className="btn " onClick={onClick} ref={ref}>
             {value}
         </button>
     ));
-    props.date === 'start'?  bookingCtx.setStartDate(date) : bookingCtx.setEndDate(date)
-    bookingCtx.setNumOfNights(bookingCtx.startDate, bookingCtx.endDate)
+
+    useEffect(() => {
+        props.date === 'start' ? bookingCtx.setStartDate(date) : bookingCtx.setEndDate(date)
+        bookingCtx.setNumOfNights(bookingCtx.startDate, bookingCtx.endDate)
+    },[bookingCtx,props.date,date])
+
+
     return (
         <DatePicker
             selected={date}
